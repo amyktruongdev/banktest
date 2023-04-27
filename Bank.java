b64e10409f88f5c9a2f26a8d1958531f5e5b4cc8
Amy Truong <amyktruongdev@gmail.com>
2023-04-26 20:55:06 -0700

    start of user prompts

--- Bank.java
+++ Bank.java
@@ -4,10 +4,16 @@ public class Bank {
         //creating object
         Account AmysAccount = new Account("3094598", 0.0, "Amy", "amyktruongdev@gmail.com", "9578045255");
 
+        //insert prompts for user
+        System.out.println("-------Welcome to the Bank of Meow!-------");
+        
+
         //testing all functions, successful !!!!!!!!!!!!!!!!! :D
         AmysAccount.DepoistMoney(1000);
         AmysAccount.Withdraw(200);
         AmysAccount.DepoistMoney(300);
         AmysAccount.Withdraw(1200);
+
+        
     }
 }
