c72fa103127617a13447ee8a07d9ced6b2de25c9
Amy Truong <amyktruongdev@gmail.com>
2023-04-26 20:38:56 -0700

    Testing funcs

--- Bank.java
+++ Bank.java
@@ -3,5 +3,11 @@ public class Bank {
     public static void main(String[] args) {
         //creating object
         Account AmysAccount = new Account("3094598", 0.0, "Amy", "amyktruongdev@gmail.com", "9578045255");
+
+        //testing all functions, successful !!!!!!!!!!!!!!!!! :D
+        AmysAccount.DepoistMoney(1000);
+        AmysAccount.Withdraw(200);
+        AmysAccount.DepoistMoney(300);
+        AmysAccount.Withdraw(1200);
     }
 }
