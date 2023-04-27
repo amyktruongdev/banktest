4aa4c7178c57e04ea608837c5ec6a556c8d11de9
Amy Truong <amyktruongdev@gmail.com>
2023-04-26 20:27:13 -0700

    Create object to test

--- Bank.java
+++ Bank.java
@@ -1,6 +1,7 @@
 
 public class Bank {
     public static void main(String[] args) {
-        
+        //creating object
+        Account AmysAccount = new Account("3094598", 0.0, "Amy", "amyktruongdev@gmail.com", "9578045255");
     }
 }
